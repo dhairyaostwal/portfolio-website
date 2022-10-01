426e3b4b76e8b1b171a628d45dd362ccca4eaf18
Dhairya <dhairya.ostwal@gmail.com>
2022-10-02 01:10:45 +0530

    Updated controllers with fetch api usage

--- controllers/blogs.controllers.js
+++ controllers/blogs.controllers.js
@@ -1,25 +1,29 @@
-import blogsJson from '../data/blogs.json' assert { type: 'json' };
+// import blogsJson from '../data/blogs.json' assert { type: 'json' };
+fetch('../data/blogs.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const blogsJson = data;
+    const blogDiv = document.createElement('div');
 
-const blogDiv = document.createElement('div');
+    for (let element = 0; element < blogsJson.length; element++) {
+      // for (const element of blogsJson) {
+      const blog = document.createElement('div');
+      const dateElement = document.createElement('h5');
+      dateElement.innerText = blogsJson[element].published_on;
+      dateElement.className = 'Blogs--Content--Month';
 
-for (let element = 0; element < blogsJson.length; element++) {
-  // for (const element of blogsJson) {
-  const blog = document.createElement('div');
-  const dateElement = document.createElement('h5');
-  dateElement.innerText = blogsJson[element].published_on;
-  dateElement.className = 'Blogs--Content--Month';
+      const paraContainer = document.createElement('p');
+      const para = document.createElement('a');
+      para.innerText = blogsJson[element].title;
+      para.href = blogsJson[element].blog_link;
+      paraContainer.appendChild(para);
+      paraContainer.className = 'Blogs--Content--Data';
 
-  const paraContainer = document.createElement('p');
-  const para = document.createElement('a');
-  para.innerText = blogsJson[element].title;
-  para.href = blogsJson[element].blog_link;
-  paraContainer.appendChild(para);
-  paraContainer.className = 'Blogs--Content--Data';
+      blog.appendChild(dateElement);
+      blog.appendChild(paraContainer);
+      blog.className = 'Blogs--Standard--Font';
+      blogDiv.appendChild(blog);
+    }
 
-  blog.appendChild(dateElement);
-  blog.appendChild(paraContainer);
-  blog.className = 'Blogs--Standard--Font';
-  blogDiv.appendChild(blog);
-}
-
-document.getElementById('Blogs--Content').appendChild(blogDiv);
+    document.getElementById('Blogs--Content').appendChild(blogDiv);
+  });
